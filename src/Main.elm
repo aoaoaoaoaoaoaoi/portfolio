0cabbd7aeb2c2f4d7b36bcfddd93865450c7fa5a
unknown <gummi.bear456aaaaa@gmail.com>
2021-01-18 02:45:41 +0900

    Add a nav tag

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,16 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, header, text)
+import Html exposing (Html, div, h1, header, nav, text)
 import Html.Attributes exposing (attribute, style)
 
 
 main : Html msg
 main =
-    header [ attribute "style" "height: 500px; background-size: auto 100%; background: url(./image/background.jpg) center center;" ] [ text "Hello!" ]
+    div []
+        [ header [ attribute "style" "height: 500px; background-size: auto 100%; background: url(./image/background.jpg) center center;" ]
+            [ h1 [ attribute "style" "position: absolute; margin: auto; top: 0; bottom: 0; left: 0; right: 0; height: 3.2rem; text-align: center" ]
+                [ text "ao's portfolio" ]
+            ]
+        , nav []
+            []
+        ]
