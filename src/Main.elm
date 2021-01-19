fd01daef1062557aa50d88d6f6b885acc32c9fc0
unknown <gummi.bear456aaaaa@gmail.com>
2021-01-20 02:08:47 +0900

    looking to separate css

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,7 @@ import Html.Attributes exposing (attribute, href, style)
 main : Html msg
 main =
     div []
-        [ header [ attribute "style" "height: 500px; background-size: auto 100%; background: url(./image/background.jpg) center center;" ]
+        [ header []
             [ h1 [ attribute "style" "position: absolute; margin: auto; top: 0; bottom: 0; left: 0; right: 0; height: 3.2rem; text-align: center" ]
                 [ text "ao's portfolio" ]
             ]
